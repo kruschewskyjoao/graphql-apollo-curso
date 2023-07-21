13d07beadbfe695d5f50074e78e701406f468e42
João Kruschewsky <joaokruschewsky@MacBook-Pro-de-Joao.local>
2023-07-21 10:40:12 -0300

    dataloader and datasources

--- src/graphql/post/datasources.js
+++ src/graphql/post/datasources.js
@@ -1,9 +1,11 @@
 import { RESTDataSource } from 'apollo-datasource-rest';
+import makePostDataLoader from './dataloaders';
 
 export class PostsApi extends RESTDataSource {
   constructor() {
     super();
     this.baseURL = process.env.API_URL + '/posts';
+    this.dataLoader = makePostDataLoader(this.getPosts.bind(this));
   }
 
   async getPosts(urlParams = {}) {
