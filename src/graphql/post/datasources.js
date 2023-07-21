0c86a32f5aac78d1f3f56315b086e9aae1b06b49
João Cerqueira <joaokruschewsky@gmail.com>
2023-07-21 13:01:28 -0300

    update branch

---
+++ src/graphql/post/datasources.js
@@ -0,0 +1,20 @@
+import { RESTDataSource } from 'apollo-datasource-rest';
+
+export class PostsApi extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = process.env.API_URL + '/posts';
+  }
+
+  async getPosts(urlParams = {}) {
+    return this.get('', urlParams, {
+      cacheOptions: { ttl: 60 },
+    });
+  }
+
+  async getPost(id) {
+    return this.get(id, undefined, {
+      cacheOptions: { ttl: 60 },
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import { ApolloServer } from 'apollo-server';
 import { context } from './graphql/context';
 import { typeDefs, resolvers } from './graphql/schema';
+import { PostsApi } from './graphql/post/datasources';
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context,
+  dataSources: () => {
+    return {
+      postApi: new PostsApi(),
+    };
+  },
 });
 
 server
